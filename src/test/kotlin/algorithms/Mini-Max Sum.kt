e569e907e776477296d57e563c265adba19b7a91
Rob Yeates <robertyeates@gmail.com>
2019-01-06 14:36:05 +0200

    another test

--- src/test/kotlin/algorithms/Mini-Max Sum.kt
+++ src/test/kotlin/algorithms/Mini-Max Sum.kt
@@ -8,6 +8,10 @@ class Test5 {
         val pair = miniMaxSum(arrayOf(7, 69, 2, 221, 8974))
         assertEquals(299, pair.first)
         assertEquals(9271, pair.second)
+
+        val secondPair = miniMaxSum(arrayOf(5, 5, 5, 5, 5))
+        assertEquals(20, secondPair.first)
+        assertEquals(20, secondPair.second)
     }
 }
 
