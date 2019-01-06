5967359b15aa9b26bd1add2ec71fd213c8612d3d
Rob Yeates <robertyeates@gmail.com>
2019-01-06 11:02:28 +0200

    passed third

--- src/main/kotlin/algorithms/Diagonal Difference.kt
+++ src/main/kotlin/algorithms/Diagonal Difference.kt
@@ -1,31 +1,16 @@
 package algorithms
 
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
 import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
-import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
 
 // Complete the diagonalDifference function below.
 fun diagonalDifference(arr: Array<Array<Int>>): Int {
     val arrayCount = arr.size
     var firstDiag = 0
-    repeat(arrayCount) {i -> firstDiag += (arr[i][i])}
     var secondDiag = 0
-    repeat(arrayCount) {i -> secondDiag += (arr[(arrayCount - 1 - i)][i])}
+    repeat(arrayCount) {i ->
+        firstDiag += (arr[i][i])
+        secondDiag += (arr[(arrayCount - 1 - i)][i])
+    }
     return  Math.abs(firstDiag.minus(secondDiag))
 }
 
