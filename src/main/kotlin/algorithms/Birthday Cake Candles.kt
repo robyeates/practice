2d06e0a333e604830f3f7ad681d5972c99d949fe
Rob Yeates <robertyeates@gmail.com>
2019-01-06 15:05:16 +0200

    solved another with a cheeky grouping by

---
+++ src/main/kotlin/algorithms/Birthday Cake Candles.kt
@@ -0,0 +1,26 @@
+package algorithms
+
+import java.util.*
+import java.util.stream.Collectors.groupingBy
+
+// Complete the birthdayCakeCandles function below.
+fun birthdayCakeCandles(ar: Array<Int>): Int {
+    return ar.toList().stream()
+        .collect(groupingBy<Int, Int> { it })
+        .toSortedMap()
+        .maxBy { it.value.size }
+        ?.value
+        ?.size!!
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = birthdayCakeCandles(ar)
+
+    println(result)
+}
