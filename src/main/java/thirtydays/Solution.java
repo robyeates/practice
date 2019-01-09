f9d6e70e9ae887f39d2ce9b4a346824b181cbb1d
Rob Yeates <robertyeates@gmail.com>
2019-01-09 10:58:35 +0200

    more guff

---
+++ src/main/java/thirtydays/Solution.java
@@ -0,0 +1,35 @@
+package thirtydays;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println (i + Integer.valueOf(args[0]));
+        System.out.println (d + Double.valueOf(args[1]));
+        System.out.println (s + args[2]);
+        /* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    }
+}
