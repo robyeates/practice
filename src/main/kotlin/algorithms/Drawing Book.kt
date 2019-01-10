c69a31001c6a24d86983b7efc5af118543f6adfc
Rob Yeates <robertyeates@gmail.com>
2019-01-10 11:15:54 +0200

    some more

---
+++ src/main/kotlin/algorithms/Drawing Book.kt
@@ -0,0 +1,32 @@
+package algorithms
+
+import java.util.*
+
+/*
+ * Complete the pageCount function below.
+ */
+fun pageCount(numberOfPages: Int, pageDesired: Int): Int {
+    if (numberOfPages > 2 && (numberOfPages - 1 == pageDesired) && numberOfPages % 2 == 0) {
+        return 1 //special case
+    } else if (numberOfPages == pageDesired || (numberOfPages - 1 == pageDesired)) {
+        return 0 //special case
+    } else if (pageDesired.toDouble() / numberOfPages.toDouble() < 0.5) { //count up
+        return (0..pageDesired step 2).fold(-1) { turns, _ -> turns.inc() }
+    } else { //count down
+        return (numberOfPages downTo pageDesired step 2).fold(-1) { turns, _ -> turns.inc() }
+    }
+    //or Math.min(pageDesired/2, numberOfPages/2 - pageDesired/2)//i.e start from front or or fewer turns start from back
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val p = scan.nextLine().trim().toInt()
+
+    val result = pageCount(n, p)
+
+    println(result)
+}
---
+++ src/test/kotlin/algorithms/Drawing Book.kt
@@ -0,0 +1,15 @@
+package algorithms
+
+import kotlin.test.assertEquals
+import org.junit.Test as test
+
+class Test19 {
+    @test fun pageCount() {
+        assertEquals(1, pageCount(6, 2))
+        assertEquals(0, pageCount(5, 4))
+        assertEquals(1, pageCount(7, 4))
+        assertEquals(1, pageCount(6, 5))
+        assertEquals(0, pageCount(2, 1))
+        assertEquals(3478, pageCount(15603, 6957))
+    }
+}
